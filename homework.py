6b53e86ac9f004851a1d8bb6e19b4e853cc10c04
Styop01 <123570855+Styop01@users.noreply.github.com>
2023-01-28 19:01:35 +0400

    Add files via upload

--- homework.py
+++ homework.py
@@ -1,6 +1,64 @@
 
-        
+#######1
+
+#x = int(input())
+#if x % 2 == 0:
+#    print("even")
+#else:
+#    print("odd")
+
+#######2
+
+#user_input = input("write a letter: ")
+#if user_input == "a" or user_input == "e" or user_input == "u" or user_input == "i" or user_input == "o":
+#    print("it is vowel")
+#else:
+#    print("it is consonant")
 
 
+#######3
+#  inp = int(input("Write A Number!!!" ))
+#if inp == 1:
+#    print(0)
+#if inp == 2:
+#    print(1)
+#if inp > 2:
+#    n = 0
+#    n1 = 1
+#    count = 0
+#    for i in range(inp - 2):
+#         count = n + n1
+#         n = n1
+#         n1 = count
+#    print (n1)
+  
+
+#######4
+
+#usr_in = int(input("Write a number: "))
+#if usr_in >= 0 and usr_in <= 9:
+#    print("THE SUM IS:", usr_in)
+#if usr_in >=10 and usr_in <= 99:
+#    num = usr_in % 10
+#    usr_in //= 10
+#    res = num + usr_in
+#    print("Result is:", res)
+#if usr_in >= 100 and usr_in <= 999:
+#    num = usr_in // 100
+#    num1 = usr_in % 100
+#    num1 //= 10
+#    usr_in %= 10
+#    res = num + num1 + usr_in
+#    print("Result in:", res)
+
+
+########5
+
+inp = int(input("Size Of The Image:" ))
+for i in range(inp): 
+    if i == inp-1 or i == 0 :
+        print(" *" * inp)
+    else:
+        print(" *" + (inp - 2) * "  " + " *" ) 
 
 
