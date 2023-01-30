16f3aa44ddc075047b5bd45efe267d9e0c7bff6e
Styop01 <123570855+Styop01@users.noreply.github.com>
2023-01-30 22:59:45 +0400

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,44 @@
+#####2
+#from random import randint
+#for i in range(50):
+#    a = randint(3, 6)
+#    print(a)
+
+#####3
+#x = int(input("first number(x):" ))
+#y = int(input("second number(y):" ))
+#sub = x - y
+#if sub < 0:
+#    sub = sub * (-1)
+#    res = sub / (x + y)
+#    print("result is:", res)
+#else:
+#    sub = sub / (x + y)
+#print("result is:", sub)
+
+#####4
+#inp = input("write a world:" )
+#s = inp[::-1]
+#if s == inp:
+#    print("it is a palindrome:")
+#else:
+#    print("it is not a palindrome:") 
+
+#####5
+#list  = [12, 45, 85, 63, 854, 999]
+#num = list[0]
+#for big_num in list:
+#    if num > big_num:
+#        big_num = num
+#print(big_num)
+
+#####6
+#inp = input()
+#reverse = inp[::-1]
+#print(reverse)
+
+#####7
+#inp = int(input("write a radius of a circle:" ))
+#s = 3.14 * inp * inp
+#print(s)
+
