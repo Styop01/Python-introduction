4d12da83f41e2d77f2392a015f8d1251c59ed7f3
Styop01 <123570855+Styop01@users.noreply.github.com>
2023-02-05 23:49:21 +0400

    Add files via upload

--- homework.py
+++ homework.py
@@ -1,13 +1,36 @@
+#####1
+#a = str(input())
+#b = str(input())
+#c = str(input())
+#d = str(input())
+#e = str(input())
+#print(a, "+", b, "+", c, "+", d, "+", e)
 
+#####2
+#inp = str(input())
+#for i in inp:
+#    if i >= '0' and i <= '9' or i == '-':
+#        if inp[1] == inp[5] == inp[9] == '-' or inp[3] == inp[7] == '-':
+#            print('valid')
+#            break
+#    else:
+#        print("invalid")
+#        break
 
+#####3
+#inp = ord(input())
+#if inp == ord('A'):
+#    print(chr(88))
+#if inp == ord('B'):
+#    print(chr(89))
+#if inp == ord('C'):
+#    print(chr(90))
+#if inp >= 68 and inp <= 90:
+#    inp -= 3
+#    print(chr(inp))
 
-########5
+#####5
+#inp = set(input())
+#print(inp)
 
-inp = int(input("Size Of The Image:" ))
-for i in range(inp): 
-    if i == inp-1 or i == 0 :
-        print(" *" * inp)
-    else:
-        print(" *" + (inp - 2) * "  " + " *" ) 
-
-
+    
