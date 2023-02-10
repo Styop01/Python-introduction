d2cd17fc52f057639af6f728519390e9e88e05b2
Styop01 <123570855+Styop01@users.noreply.github.com>
2023-02-11 01:55:16 +0400

    Add files via upload

--- homework1.py
+++ homework1.py
@@ -1,44 +1,44 @@
 #####2
-#from random import randint
-#for i in range(50):
-#    a = randint(3, 6)
-#    print(a)
+#ls  = [
+#       [1, 2, 3, 4],
+#       [5, 6, 7, 8],
+#       [9, 10, 11, 12],
+#       [13, 14, 15, 16]
+#       ]
+#ls[3].reverse()
+#ls[2].reverse()
+#ls[1].reverse()
+#ls[0].reverse()
+#ls.reverse()
+#print(ls)
 
 #####3
-#x = int(input("first number(x):" ))
-#y = int(input("second number(y):" ))
-#sub = x - y
-#if sub < 0:
-#    sub = sub * (-1)
-#    res = sub / (x + y)
-#    print("result is:", res)
-#else:
-#    sub = sub / (x + y)
-#print("result is:", sub)
+#ls = [[1, 2, 3],
+#      [4, 5, 6],
+#      [7, 8, 9]]
+#new_ls = ls.copy()
+#new_ls[0] = [ls[0][0], ls[1][0], ls[2][0]]
+#new_ls[1] = [ls[0][1], ls[1][1], ls[2][1]]
+#new_ls[2] = [ls[0][2], ls[1][2], ls[2][2]]
+#print(new_ls)
 
 #####4
-#inp = input("write a world:" )
-#s = inp[::-1]
-#if s == inp:
-#    print("it is a palindrome:")
-#else:
-#    print("it is not a palindrome:") 
+#inp = int(input())
+#if inp == 1:
+#    print('yes')
+#num = 2
+#start = 1
+#ls = []
+#for i in range(15):
+#    start *= num
+#    ls.append(start)
+#    if inp == start:
+#        print('yes')
+#        break
 
-#####5
-#list  = [12, 45, 85, 63, 854, 999]
-#num = list[0]
-#for big_num in list:
-#    if num > big_num:
-#        big_num = num
-#print(big_num)
 
-#####6
-#inp = input()
-#reverse = inp[::-1]
-#print(reverse)
 
-#####7
-#inp = int(input("write a radius of a circle:" ))
-#s = 3.14 * inp * inp
-#print(s)
 
+ 
+
+        
